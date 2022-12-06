2871632786fb8bb3da8fca060f1c8a1ebf68f86c
megan.archibald@umontana.edu <megan.archibald@umontana.edu>
2022-12-05 22:02:41 -0700

    retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+
+class Constants {
+    companion object{
+        const val API_KEY = "83b04a390d7e44328730681ab15d8abd"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
