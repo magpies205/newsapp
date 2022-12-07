ff822561a8935c4c4c33d6ec97c7e949db21e770
megan.archibald@umontana.edu <megan.archibald@umontana.edu>
2022-12-06 21:06:59 -0700

    viewmodels

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
@@ -1,7 +1,17 @@
 package com.androiddevs.mvvmnewsapp.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import com.androiddevs.mvvmnewsapp.R
 import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.ui.MainActivity
+import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+    }
 }
