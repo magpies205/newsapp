0ca2fbbcd2494dd9fae8610940d820f701cf89d6
megan.archibald@umontana.edu <megan.archibald@umontana.edu>
2022-12-06 21:46:51 -0700

    newsshowingup

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.androiddevs.mvvmnewsapp.repository
 
 import com.androiddevs.mvvmnewsapp.db.ArticleDataBase
+import com.androiddevs.mvvmnewsapp.ui.api.retrofitinstance
+
 
 class NewsRepository(
     val db: ArticleDataBase
 ) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        retrofitinstance.api.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
@@ -1,17 +1,66 @@
 package com.androiddevs.mvvmnewsapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.androiddevs.mvvmnewsapp.R
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.androiddevs.mvvmnewsapp.adapters.NewsAdapter
+import com.androiddevs.mvvmnewsapp.models.newsresponse
 import com.androiddevs.mvvmnewsapp.ui.MainActivity
 import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import com.androiddevs.mvvmnewsapp.util.Resource
+import androidx.lifecycle.Observer
+
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "BreakingNewsFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer{ response ->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let{ newsresponse ->
+                        newsAdapter.differ.submitList(newsresponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let{ message ->
+                        Log.e(TAG,"There was an error: $message")
+                    }
+                }
+
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+
+        })
+    }
+    private fun hideProgressBar(){
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply{
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
